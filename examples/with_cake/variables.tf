f99f17d0035fdd31e5717c564613bcab8f90561c
Sean Dowd <sdowd@nexient.com>
2023-08-25 14:44:02 -0500

    chore: update .tool-versions
    chore: update copyright year

--- examples/with_cake/variables.tf
+++ examples/with_cake/variables.tf
@@ -1,4 +1,4 @@
-// Copyright 2022 Nexient LLC
+// Copyright 2023 Nexient LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
